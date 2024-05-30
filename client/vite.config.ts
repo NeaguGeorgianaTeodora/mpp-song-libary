f9003d5c6e7faacc26dbcb59fd8508d3c07358a3
Rares Modure <m.rares956@yahoo.com>
2024-05-30 17:04:13 +0200

    Added Dockerfgile and compose service for react client app

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,17 +1,17 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-},
-  server: {port: 3000},
-})
+    plugins: [react()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+    },
+    server: {port: 3000},
+});
 
 /*
 /// <reference types="vitest" />
